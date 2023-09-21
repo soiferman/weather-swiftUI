feb0bfbebefe85480c9969ef2457ca0b6073c97e
Max Soiferman <sooiferman@gmail.com>
2023-09-21 19:44:48 -0400

    Initial commit

---
+++ Weather SwiftUI/ContentView.swift
@@ -0,0 +1,132 @@
+//
+//  ContentView.swift
+//  Weather SwiftUI
+//
+//  Created by Max Soiferman on 19/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        ZStack {
+            BackgroundView(isNight: $isNight)
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
+                MainWeatherView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",
+                                temperature: 76)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temaperature: 76)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temaperature: 88)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind",
+                                   temaperature: 55)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temaperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.rain.fill",
+                                   temaperature: 24)
+                }
+                
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                }
+                
+                Spacer()
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temaperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temaperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient:
+                        Gradient(colors: [isNight ? .black : .blue,
+                                          isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom, 40)
+    }
+}
+
---
+++ Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by Max Soiferman on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
